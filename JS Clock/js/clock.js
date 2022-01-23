8058d61f8ef8ebcd7ef7bc9438bbf9cfa9a6790a
RayDugan <raymond.j.dugan@gmail.com>
2022-01-23 17:21:43 -0600

    Created and finihsed a JavaScript Clock. All HTML, CSS and JS was done by Raymond Dugan

--- JS Clock/js/clock.js
+++ JS Clock/js/clock.js
@@ -1,12 +1,30 @@
-let setTime = document.getElementById('setTime');
-let myTime = new Date();
+const timeEl = document.getElementById('time-element');
+const dateEl = document.getElementById('date-element');
+const locale = navigator.language;
 
-
-    const hours = myTime.getHours();
-    const mins = myTime.getMinutes();
-    const secs = myTime.getSeconds();
-    setTime.textContent = `${hours}:${mins}:${secs}`
-    console.log(`${hours}:${mins}:${secs}`)
-
+function displayDate (el, lang){
+    const dateOptions = {
+        weekday: 'long',
+        month: 'long',
+        day: 'numeric',
+        year: 'numeric'
+    }
+    el.innerText = new Intl.DateTimeFormat(lang, dateOptions).format(new Date())
+}
+displayDate(dateEl, locale);
+function setCurrentTime (el, lang) {
+    const timeFormat = function (){
+        const time = new Date()
+        const timeOptions = {
+            hour: 'numeric',
+            minute: 'numeric',
+            second: 'numeric',
+        }
+        el.innerText = time.toLocaleString(lang, timeOptions)
+    }
+    timeFormat();
+    setInterval(timeFormat, 1000);
+}
+setCurrentTime(timeEl, locale)
 
 
